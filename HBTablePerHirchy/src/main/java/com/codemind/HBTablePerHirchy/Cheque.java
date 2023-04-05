b0fcfe9f62065703c26d445fa831a30d46a65e76
kiran <kirankadam1112@gmail.com>
2023-04-05 17:26:42 +0530

    Tableperhirchy

---
+++ HBTablePerHirchy/src/main/java/com/codemind/HBTablePerHirchy/Cheque.java
@@ -0,0 +1,27 @@
+package com.codemind.HBTablePerHirchy;
+
+import javax.persistence.Column;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Cheque")
+public class Cheque extends Payment{
+	
+	
+
+	@Column(name = "ChequeType")
+	private String ChequeType;
+
+	public String getChequeType() {
+		return ChequeType;
+	}
+
+	public void setChequeType(String chequeType) {
+		ChequeType = chequeType;
+	}
+	
+
+		
+}
---
+++ HBTablePerHirchy/src/main/java/com/codemind/HBTablePerHirchy/CreditCard.java
@@ -0,0 +1,26 @@
+package com.codemind.HBTablePerHirchy;
+
+import javax.persistence.Column;
+
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("creditCard")
+public class CreditCard extends Payment{
+	
+	@Column(name = "CreditCardType")
+	private String CreditCardType;
+	
+
+	public String getCreditCardType() {
+		return CreditCardType;
+	}
+
+	public void setCreditCardType(String creditCardType) {
+		CreditCardType = creditCardType;
+	}
+	
+		
+}
